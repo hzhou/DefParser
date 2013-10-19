8e9545aae3a60f64047917a7f79c326f799c634b
Hui Zhou <hzhou@hzsolutions.net>
2013-10-19 13:58:44 -0500

    modified:   deflib/input_buffer.def 	new file:   deflib/parse_tree.def 	new file:   deflib/parser_c.def 	deleted:    deflib/parser_inc.def 	new file:   deflib/parser_perl.def 	modified:   deflib/regex_vm.def 	deleted:    macro_regex/build_lex.def 	modified:   macro_regex/build_nfa.def 	modified:   macro_regex/first_set.def 	modified:   macro_regex/load_grammar.def 	new file:   macro_regex/make_parser.def 	modified:   macro_regex/parse_regex.def 	modified:   macro_regex/parse_rule.def 	modified:   macro_regex/print_rule.def 	modified:   macro_regex/vm.def 	modified:   macro_regex/write_rule.def 	new file:   macro_regex/write_rule_c.def 	deleted:    make_parser.def 	new file:   make_parser_c.def 	new file:   make_parser_perl.def 	modified:   set_install.pl 	modified:   test_c/c.grammar 	new file:   test_c/c/test.def 	new file:   test_c/perl/test.def 	new file:   test_c/test.c 	modified:   test_calculator/test.def

--- set_install.pl
+++ set_install.pl
@@ -3,11 +3,14 @@ if (!-d $base){
     die "Usage: $0 INSTALL_BASE_DIR\n";
 }
 
+my @allpl=glob("out/*.pl");
 my @alldeflib=glob("deflib/*.def");
 open Out, ">>Makefile" or die;
 print Out "\n";
-print Out "install: out/make_parser.pl ", join(' ', @alldeflib), "\n";
-print Out "\tinstall -m 0755 out/make_parser.pl $base/bin\n";
+print Out "install: ",join(' ', @allpl),' ',join(' ', @alldeflib),"\n";
+foreach my $pl (@allpl){
+    print Out "\tinstall -m 0755 $pl $base/bin\n";
+}
 print Out "\tmkdir -p $base/lib/MyDef/DefParser\n";
 foreach my $lib (@alldeflib){
     print Out "\tinstall -m 0744 $lib $base/lib/MyDef/DefParser\n";
